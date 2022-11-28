0b7f33952138494883e80099a439e4fae61c80d3
Jordan Leigh <AlwaysBCoding@gmail.com>
2022-11-28 02:23:30 -0500

    adds moves for the starters

--- src/scripts/scrape-move-idents.rb
+++ src/scripts/scrape-move-idents.rb
@@ -1,13 +1,18 @@
 require "nokogiri"
 require "watir"
 
-pokemon = "tsareena"
+pokemon = ARGV[0]
 url = "https://www.smogon.com/dex/sv/pokemon/#{pokemon}/"
 
 browser = Watir::Browser.new :chrome, headless: true
-browser.goto("url")
+browser.goto(url)
 
-sleep 3
+sleep 2
 
 doc = Nokogiri::HTML.parse(browser.html)
-puts doc.css(".DexTable .MoveRow")
+move_rows = doc.css(".DexTable .MoveRow")
+
+move_rows.each do |move_row|
+  move = move_row.at_css(".MoveRow-name").text
+  p move.downcase.split(/\s+/).join("-")
+end
